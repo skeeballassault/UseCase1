4eb18cb26fbfab798b179c5e164a4fc15a3328c5
Rafael Vinluan <rafael.vinluan@gmail.com>
2018-10-21 23:17:04 -0400

    git push

---
+++ src/alphacare/submittable/patientMedicalHistoryController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alphacare.submittable;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+
+
+/**
+ *
+ * @author rafael
+ */
+public class patientMedicalHistoryController implements Initializable {
+
+    @FXML
+    private ListView<String> listView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+        ObservableList<String> data = FXCollections.observableArrayList(
+                "Heart Transplant", "Annual G.P.Check Up","Annual Dental Check Up","Annual Cancer Screening",
+                "Heart Transplant", "Annual G.P.Check Up","Annual Dental Check Up","Annual Cancer Screening",
+                "Heart Transplant", "Annual G.P.Check Up","Annual Dental Check Up","Annual Cancer Screening",
+                "Heart Transplant", "Annual G.P.Check Up","Annual Dental Check Up","Annual Cancer Screening",
+                "Heart Transplant", "Annual G.P.Check Up","Annual Dental Check Up","Annual Cancer Screening",
+
+        );
+        listView.setItems(data);
+    }
+
+
+}
