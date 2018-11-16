b2f2e33773d5776b409e945f0714dee621d62a3f
Rafael Vinluan <rafael.vinluan@gmail.com>
2018-11-16 13:23:17 -0500

    working patient history

--- src/alphacare/submittable/patientMedicalHistoryController.java
+++ src/alphacare/submittable/patientMedicalHistoryController.java
@@ -5,26 +5,18 @@
  */
 package alphacare.submittable;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.TreeMap;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
+
 
 
 
@@ -34,32 +26,43 @@ import javafx.stage.Stage;
  */
 public class patientMedicalHistoryController implements Initializable {
 
-    ObservableList data = FXCollections.observableArrayList();
-            
+    ObservableList dataKey = FXCollections.observableArrayList();
+    TreeMap<String, String> entries = new TreeMap<String, String>();
+
     @FXML
-    private ListView<String> historyList ;
-    private TextField historyTitle ;
+    private ListView<String> historyList;
+    @FXML
+    private Label historyTitle;
+    @FXML
+    private Label historyEntry;
     
     @Override
     public void initialize(URL url, ResourceBundle rb){
         loadData();
     }
     private void loadData(){
-        String title20181115 = "2018-11-15\tHeart Transplant";
-        String title20181001 = "2018-10-01\tAnnual G.P.Check Up";
-        String title20180515 = "2018-05-15\tAnnual Dental Check Up";
-        String title20180105 = "2018-01-05\tAnnual Cancer Screening";
-        data.addAll(title20181115,title20181001,title20180515,title20180105);
-        historyList.getItems().addAll(data);
+        
+        entries.put("2018-11-15 Heart Transplant", "Cardiac Surgeon Doctor Shoe took the heart of a pig and performed a 93 hour surgery on you!");
+        entries.put("2018-10-01 Annual G.P.Check Up", "Doctor Banana found that you have no heart. Sounds like my ex-wife");
+        entries.put("2018-05-15 Annual Dental Check Up", "Dr. Gums found your teeth are great.");
+        entries.put("2018-01-05 Annual Cancer Screening", "No cancer here!");
+        
+        Set<String> keys = entries.keySet();
+        for (String key : keys) {
+            dataKey.addAll(key);
+        }
+        historyList.getItems().addAll(dataKey);
     }
 
     @FXML
     private void listClick(MouseEvent event) {
         String entryTitle = historyList.getSelectionModel().getSelectedItem();
+        
         if (entryTitle == null || entryTitle.isEmpty()){
             historyTitle.setText("Nothing Selected"); 
         } else {
             historyTitle.setText(entryTitle);
+            historyEntry.setText(entries.get(entryTitle));
         }
     }
 }
