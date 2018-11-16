59bf63a97be73e8861e8e18a7f0eb0bfa6b6cac2
Rafael Vinluan <rafael.vinluan@gmail.com>
2018-11-16 12:14:03 -0500

    add mouseevent (doesn't work)

--- src/alphacare/submittable/patientMedicalHistoryController.java
+++ src/alphacare/submittable/patientMedicalHistoryController.java
@@ -22,6 +22,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -33,19 +34,32 @@ import javafx.stage.Stage;
  */
 public class patientMedicalHistoryController implements Initializable {
 
+    ObservableList data = FXCollections.observableArrayList();
+            
     @FXML
-    private ListView<String> listView;
-
+    private ListView<String> historyList ;
+    private TextField historyTitle ;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb){
-        ObservableList<String> data = FXCollections.observableArrayList(
-                "Heart Transplant", "Annual G.P.Check Up","Annual Dental Check Up","Annual Cancer Screening",
-                "Heart Transplant", "Annual G.P.Check Up","Annual Dental Check Up","Annual Cancer Screening",
-                "Heart Transplant", "Annual G.P.Check Up","Annual Dental Check Up","Annual Cancer Screening",
-                "Heart Transplant", "Annual G.P.Check Up","Annual Dental Check Up","Annual Cancer Screening",
-                "Heart Transplant", "Annual G.P.Check Up","Annual Dental Check Up","Annual Cancer Screening");
-        listView.setItems(data);
+        loadData();
+    }
+    private void loadData(){
+        String title20181115 = "2018-11-15\tHeart Transplant";
+        String title20181001 = "2018-10-01\tAnnual G.P.Check Up";
+        String title20180515 = "2018-05-15\tAnnual Dental Check Up";
+        String title20180105 = "2018-01-05\tAnnual Cancer Screening";
+        data.addAll(title20181115,title20181001,title20180515,title20180105);
+        historyList.getItems().addAll(data);
     }
 
-
+    @FXML
+    private void listClick(MouseEvent event) {
+        String entryTitle = historyList.getSelectionModel().getSelectedItem();
+        if (entryTitle == null || entryTitle.isEmpty()){
+            historyTitle.setText("Nothing Selected"); 
+        } else {
+            historyTitle.setText(entryTitle);
+        }
+    }
 }
